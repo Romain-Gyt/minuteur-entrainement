091e970021c1d5c6fbd7ca8e6c6f76eef109349a
romain-gyt <guyenot.romain@protonmail.com>
2025-11-10 15:31:10 +0100

    Initial commit - Vue + Tailwind base clean

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="min-h-screen grid place-items-center bg-neutral-900 text-white">
+    <div class="text-center space-y-2">
+      <h1 class="text-4xl font-bold">Vue + Tailwind v4 ✅</h1>
+      <p class="text-neutral-400">
+        Si tu lis ça en blanc sur fond sombre, c’est bon.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
