e208e03fda1e094599e0bdb00b785c7df1907bf8
romain-gyt <guyenot.romain@protonmail.com>
2025-11-11 15:37:03 +0100

    mise en place de la pwa

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    // Optionnel: afficher un toast "Nouvelle version dispo" puis updateSW(true)
+    updateSW(true);
+  },
+  onOfflineReady() {
+    // Optionnel: toast "L’app est prête hors-ligne"
+    console.log("PWA prête hors-ligne");
+  },
+});
+
 createApp(App).use(router).mount("#app");
